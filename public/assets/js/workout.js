8302b7eef41a3f34beaaede2bc42c61a1ae64cdb
Maura Slavin <MauraMSlavin@gmail.com>
2020-01-25 18:43:30 -0500

    Displays current workout name on workout html page.

--- public/assets/js/workout.js
+++ public/assets/js/workout.js
@@ -1,3 +1,21 @@
+function getCurrentWO() {
+  // get name of current workout from workouts collection
+  $.get("api/workouts/current", workout => {
 
+    if (workout === null) {
+      console.log(
+        "ERROR:  There should be a current workout, and there isn't."
+      );
+    } else {
+      $("#workoutName").text(workout.name);
 
+      return workout;
+    }
+  });
+} // end of getCurrentWO function
 
+$(document).ready(() => {
+  console.log("In workout.js, document ready");
+  // get the current workout (also displays name on page)
+  let workout = getCurrentWO();
+});
